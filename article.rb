e25a3ef6a1c94005f720ae8392e26cc5a9bda855
nthokiesther09 <nthokiesther09@gmail.com>
2023-02-19 21:07:41 +0300

    created Article class

--- article.rb
+++ article.rb
@@ -23,3 +23,21 @@ class Magazine
     end
 
 end
+
+class Article
+
+    attr_reader :author, :magazine, :title
+
+    @@all = []
+    
+    def initialize(author, magazine, title)
+        @author = author
+        @magazine = magazine
+        @title = title
+        @@all<< self
+    end
+    def self.all
+        @@all
+    end
+    
+end
